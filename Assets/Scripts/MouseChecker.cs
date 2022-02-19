db61ee7b9ff4954594ac40dfd4a7ddbf84662ad4
tixvage <mel10h3421@gmail.com>
2022-02-19 14:43:05 -0800

    intractable objectler bitti (umarım)

---
+++ Assets/Scripts/MouseChecker.cs
@@ -0,0 +1,46 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MouseChecker : MonoBehaviour
+{
+    Camera m_camera;
+    GameObject m_lastHittedObject;
+    GameObject m_newHittedObject;
+
+    void Start() {
+        m_camera = Camera.main;
+    }
+
+    void Update() {
+        if (Input.GetMouseButtonDown(0)) {
+            RaycastHit2D hit = Physics2D.Raycast(m_camera.ScreenToWorldPoint(Input.mousePosition), Vector2.zero, Mathf.Infinity, LayerMask.GetMask("Default"));
+
+            if (hit) {
+                GameObject hittedObject = hit.collider.gameObject;
+
+                if (hittedObject.CompareTag("Intractable")) {
+                    m_newHittedObject = hittedObject;
+
+                    Transform intractable = hittedObject.transform.Find("Intractable");
+                    if (intractable){
+                        intractable.gameObject.SetActive(true);
+                    }
+
+                    CheckHittedObjects();
+                }
+            }
+        }
+    }
+
+    void CheckHittedObjects() {
+        if (m_lastHittedObject) {
+            if (!ReferenceEquals(m_lastHittedObject, m_newHittedObject)) {
+                m_lastHittedObject.transform.Find("Intractable").gameObject.SetActive(false);
+            }
+            m_lastHittedObject = m_newHittedObject;
+        } else {
+            m_lastHittedObject = m_newHittedObject;
+        }
+    }
+}
