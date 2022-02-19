6229697d0f67ea1a4365da85db86e83892082ff3
Kenpar <kenpar@kodrehberim.com>
2022-02-19 20:14:36 +0300

    basic room change

--- Assets/Scripts/RoomChanger.cs
+++ Assets/Scripts/RoomChanger.cs
@@ -5,48 +5,50 @@ using Cinemachine;
 
 public enum Direction {
     LEFT,
-    RIGHT
+    RIGHT,
+    UP,
+    DOWN
 }
 
 public class RoomChanger : MonoBehaviour
 {
-    int m_currentRoom = 0;
+    [SerializeField] RoomController m_currentRoom;
 
     public void ChangeRoom(Direction direction) {
         switch (direction) {
             case Direction.LEFT:
-                Left();
+                if (m_currentRoom.LeftRoom != null) ChangeRoom(m_currentRoom.LeftRoom); 
                 break;
-            
             case Direction.RIGHT:
-                Right();
+                if (m_currentRoom.RightRoom != null) ChangeRoom(m_currentRoom.RightRoom);
+                break;
+            case Direction.UP:
+                if (m_currentRoom.UpRoom != null) ChangeRoom(m_currentRoom.UpRoom);
+                break;
+            case Direction.DOWN:
+                if (m_currentRoom.DownRoom != null) ChangeRoom(m_currentRoom.DownRoom);
                 break;
         }
     }
 
-    void Left() {
-        if (GameObject.Find("VirtualCamera" + (m_currentRoom - 1).ToString())){
-            m_currentRoom--;
-            GameObject.Find("VirtualCamera" + m_currentRoom.ToString()).GetComponent<CinemachineVirtualCamera>().enabled= true;
-            GameObject.Find("VirtualCamera" + (m_currentRoom+1).ToString()).GetComponent<CinemachineVirtualCamera>().enabled= false;
-        }
-    }
-
-    void Right() {
-        if (GameObject.Find("VirtualCamera" + (m_currentRoom + 1).ToString())){
-            m_currentRoom++;
-            GameObject.Find("VirtualCamera" + m_currentRoom.ToString()).GetComponent<CinemachineVirtualCamera>().enabled= true;
-            GameObject.Find("VirtualCamera" + (m_currentRoom-1).ToString()).GetComponent<CinemachineVirtualCamera>().enabled= false;
-        }
+    void ChangeRoom(RoomController room) {
+        m_currentRoom.VirtualCam.SetActive(false);
+        m_currentRoom = room;
+        m_currentRoom.VirtualCam.SetActive(true);
     }
 
     void Update() {
-        if (Input.GetKeyDown(KeyCode.L)) {
+        if (Input.GetKeyDown(KeyCode.LeftArrow)) {
             ChangeRoom(Direction.LEFT);
         }
-        
-        else if (Input.GetKeyDown(KeyCode.R)) {
+        else if (Input.GetKeyDown(KeyCode.RightArrow)) {
             ChangeRoom(Direction.RIGHT);
+        }
+        else if (Input.GetKeyDown(KeyCode.UpArrow)) {
+            ChangeRoom(Direction.UP);
+        }
+        else if (Input.GetKeyDown(KeyCode.DownArrow)) {
+            ChangeRoom(Direction.DOWN);
         }
     }
 }
---
+++ Assets/Scripts/RoomController.cs
@@ -0,0 +1,10 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RoomController : MonoBehaviour
+{
+    public RoomController LeftRoom, RightRoom, UpRoom, DownRoom;
+    public GameObject VirtualCam;
+    public GameObject RoomObjects;
+}
