d8f7c5a4e4bda0dc3d6b0a634061281e628ea7d7
MFaik <m.faikekclr@gmail.com>
2022-02-20 13:47:36 +0300

    editor fix for mouse controller

--- Assets/Scripts/MouseController.cs
+++ Assets/Scripts/MouseController.cs
@@ -34,24 +34,22 @@ public class MouseController : MonoBehaviour
 
         m_mainCamera = Camera.main;
         
-        m_cursorSetPoint = new Vector2(Screen.width/2f,Screen.height/2f);
+        m_cursorSetPoint = new Vector2(Screen.currentResolution.width/2f,Screen.currentResolution.height/2f);
         SetCursorPos((int)m_cursorSetPoint.x,(int)m_cursorSetPoint.y);
     }
 
     void Update() {
+#if UNITY_EDITOR
+        m_rigidbody.MovePosition(Vector3.MoveTowards(transform.position, Camera.main.ScreenToWorldPoint(Input.mousePosition), 5));
+#else
         Point cursorPos = new Point();
         GetCursorPos(out cursorPos);
-        if(new Vector2(cursorPos.X,cursorPos.Y) != m_cursorSetPoint){
-            Debug.Log(new Vector2(cursorPos.X,cursorPos.Y) + " - - " + m_cursorSetPoint);
-            Debug.Log(m_mainCamera.ScreenToWorldPoint(new Vector2(cursorPos.X,cursorPos.Y)-m_cursorSetPoint));
-        }
 
-        Debug.Log(Time.deltaTime);
         Vector3 mouseMovementVector = m_mainCamera.ScreenToWorldPoint(new Vector2(cursorPos.X,cursorPos.Y))-m_mainCamera.ScreenToWorldPoint(m_cursorSetPoint);
         mouseMovementVector *= new Vector2(1,-1);
         m_rigidbody.MovePosition(transform.position + mouseMovementVector);
         SetCursorPos((int)m_cursorSetPoint.x,(int)m_cursorSetPoint.y);
-
+#endif
         MousePosition = transform.position;
     }
 }
